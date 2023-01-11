3f8335ddfbfc7633e236cf7e2366565cb583f5fe
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-01-11 21:39:03 +0100

    no auto

--- lua/config/lsp/mason.lua
+++ lua/config/lsp/mason.lua
@@ -15,7 +15,7 @@ function M.setup()
         ensure_installed = {
             "sumneko_lua"
         },
-        automatic_installation = true
+        automatic_installation = false
     }
     require("mason").setup(mason_options)
     require("mason-lspconfig").setup(mason_lspconfig_options)
