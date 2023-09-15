878ff57c7bdd3965aba821d43bfd7714597af17b
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-09-15 20:03:57 +0200

    lazy mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -134,7 +134,7 @@ return {
     },
     {
         'williamboman/mason-lspconfig.nvim',
-        --event = 'VeryLazy',
+        event = 'UIEnter',
         dependencies = { 'williamboman/mason.nvim' },
         config = function()
             require("mason-lspconfig").setup()
