3209ad9fe4346073bdbfaa3f7a1237d738cc9909
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-09-15 20:05:24 +0200

    bad lazy mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -134,7 +134,7 @@ return {
     },
     {
         'williamboman/mason-lspconfig.nvim',
-        event = 'UIEnter',
+        event = 'BufReadPost',
         dependencies = { 'williamboman/mason.nvim' },
         config = function()
             require("mason-lspconfig").setup()
