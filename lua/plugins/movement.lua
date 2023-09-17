613d4f8bf6fd25d6f04df9965c2d3cc08b422524
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-09-17 16:34:32 +0200

    lazy nvim-spider after fixing the map lag

--- lua/plugins/movement.lua
+++ lua/plugins/movement.lua
@@ -14,19 +14,19 @@ return {
     {
         "chrisgrieser/nvim-spider",
         init = function()
-            vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-            vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-            vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-            vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
+            -- vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+            -- vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+            -- vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+            -- vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
         end,
-        -- keys = {
-        --     { 'w', function() require('spider').motion('w') end },
-        --     { 'w', function() require('spider').motion('w') end, mode = 'x' },
-        --     { 'e', function() require('spider').motion('e') end },
-        --     { 'e', function() require('spider').motion('e') end, mode = 'x' },
-        --     { 'b', function() require('spider').motion('b') end },
-        --     { 'b', function() require('spider').motion('b') end, mode = 'x' },
-        -- }
+        keys = {
+            { 'w', function() require('spider').motion('w') end },
+            { 'w', function() require('spider').motion('w') end, mode = 'x' },
+            { 'e', function() require('spider').motion('e') end },
+            { 'e', function() require('spider').motion('e') end, mode = 'x' },
+            { 'b', function() require('spider').motion('b') end },
+            { 'b', function() require('spider').motion('b') end, mode = 'x' },
+        }
     },
     {
         'jinh0/eyeliner.nvim',
