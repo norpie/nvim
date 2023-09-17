a3f1bd05510e6e96919c11b60b35c1e49a9ca156
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-09-17 12:57:50 +0200

    fixed conflict, back no longer lags

--- lua/plugins/movement.lua
+++ lua/plugins/movement.lua
@@ -13,14 +13,20 @@ return {
     },
     {
         "chrisgrieser/nvim-spider",
-        keys = {
-            { 'w', function() require('spider').motion('w') end },
-            { 'w', function() require('spider').motion('w') end, mode = 'x' },
-            { 'e', function() require('spider').motion('e') end },
-            { 'e', function() require('spider').motion('e') end, mode = 'x' },
-            { 'b', function() require('spider').motion('b') end },
-            { 'b', function() require('spider').motion('b') end, mode = 'x' },
-        }
+        init = function()
+            vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+            vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+            vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+            vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
+        end,
+        -- keys = {
+        --     { 'w', function() require('spider').motion('w') end },
+        --     { 'w', function() require('spider').motion('w') end, mode = 'x' },
+        --     { 'e', function() require('spider').motion('e') end },
+        --     { 'e', function() require('spider').motion('e') end, mode = 'x' },
+        --     { 'b', function() require('spider').motion('b') end },
+        --     { 'b', function() require('spider').motion('b') end, mode = 'x' },
+        -- }
     },
     {
         'jinh0/eyeliner.nvim',
@@ -28,7 +34,7 @@ return {
         config = function()
             require('eyeliner').setup {
                 highlight_on_key = false, -- show highlights only after keypress
-                dim = false    -- dim all other characters if set to true (recommended!)
+                dim = false               -- dim all other characters if set to true (recommended!)
             }
         end
     },
