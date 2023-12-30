7c5be8b5f7fe8e01b12df0fc16247900babf17a9
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-30 09:20:18 +0100

    added laravel and polyglot, changed php to intelphense

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,7 +17,34 @@ return {
             require('lsp.lspconfig').setup()
         end
     },
-    { 'github/copilot.vim' },
+    {
+        'sheerun/vim-polyglot'
+    },
+    {
+        'github/copilot.vim'
+    },
+    {
+        "adalessa/laravel.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "tpope/vim-dotenv",
+            "MunifTanjim/nui.nvim",
+        },
+        cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+        keys = {
+            { "<leader>la", ":Laravel artisan<cr>" },
+            { "<leader>lr", ":Laravel routes<cr>" },
+            { "<leader>lm", ":Laravel related<cr>" },
+        },
+        event = { "VeryLazy" },
+        config = {
+            features = {
+                null_ls = {
+                    enable = false,
+                },
+            },
+        },
+    },
     -- {
     --     "zbirenbaum/copilot-cmp",
     --     config = function()
