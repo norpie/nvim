256b3b81e6a2ee2eb7d995107c98b76aff8fe906
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2024-12-23 11:55:04 +0100

    feat: lsp formatting better, added powershell

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -44,18 +44,30 @@ function M.capabilities()
 end
 
 function M.setup_formatter(client, buffer)
-    if client.server_capabilities.documentFormattingProvider then
-        vim.keymap.set('n', '<leader>f', function()
-            vim.lsp.buf.format { async = true }
-        end, { buffer = buffer, noremap = true, silent = true, desc = 'Format' })
-    else
-        vim.keymap.set(
-            'n',
-            '<leader>f',
-            '<cmd>Neoformat<CR>',
-            { buffer = buffer, noremap = true, silent = true, desc = 'Format' }
-        )
-    end
+    -- if client.server_capabilities.documentFormattingProvider then
+    --     vim.keymap.set('n', '<leader>f', function()
+    --         vim.lsp.buf.format { async = true }
+    --     end, { buffer = buffer, noremap = true, silent = true, desc = 'Format' })
+    -- else
+    --     vim.keymap.set(
+    --         'n',
+    --         '<leader>f',
+    --         '<cmd>Neoformat<CR>',
+    --         { buffer = buffer, noremap = true, silent = true, desc = 'Format' }
+    --     )
+    -- end
+    vim.keymap.set(
+        'n',
+        '<leader>f',
+        function()
+            -- Try to use lsp formatter first, if it fails, use neoformat
+            local ok, _ = pcall(vim.lsp.buf.format, { async = true })
+            if not ok then
+                vim.cmd 'Neoformat'
+            end
+        end,
+        { buffer = buffer, noremap = true, silent = true, desc = 'Format' }
+    )
 end
 
 function M.on_attach(client, buffer)
@@ -75,6 +87,7 @@ function M.on_attach(client, buffer)
 end
 
 function M.setup(_)
+    -- LSP servers --
     local servers = {
         'cssls',
         'emmet_ls',
@@ -83,6 +96,7 @@ function M.setup(_)
         'jsonls',
         'ts_ls',
         'tailwindcss',
+        'powershell_es',
         'pyright',
         'texlab',
         'clangd',
@@ -115,6 +129,14 @@ function M.setup(_)
             }
         end
     end
+    -- Manual Servers --
+    lspconfig.powershell_es.setup {
+        on_attach = M.on_attach,
+        capabilities = M.capabilities(),
+        -- bundle is at ~/repos/powershell-es
+        bundle_path = vim.fn.expand('~/repos/powershell-es'),
+    }
+    -- Custom servers --
     local custom_servers = {
         'surql',
     }
