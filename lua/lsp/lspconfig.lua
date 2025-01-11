292bda4ba5f967df3af422b8d6f6562a6f313785
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-11 18:40:02 +0100

    feat: added nil ls config

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -44,30 +44,16 @@ function M.capabilities()
 end
 
 function M.setup_formatter(client, buffer)
-    -- if client.server_capabilities.documentFormattingProvider then
-    --     vim.keymap.set('n', '<leader>f', function()
-    --         vim.lsp.buf.format { async = true }
-    --     end, { buffer = buffer, noremap = true, silent = true, desc = 'Format' })
-    -- else
-    --     vim.keymap.set(
-    --         'n',
-    --         '<leader>f',
-    --         '<cmd>Neoformat<CR>',
-    --         { buffer = buffer, noremap = true, silent = true, desc = 'Format' }
-    --     )
-    -- end
-    vim.keymap.set(
-        'n',
-        '<leader>f',
-        function()
-            -- Try to use lsp formatter first, if it fails, use neoformat
+    vim.keymap.set('n', '<leader>f', function()
+        if client and client.supports_method 'textDocument/formatting' then
             local ok, _ = pcall(vim.lsp.buf.format, { async = true })
-            if not ok then
-                vim.cmd 'Neoformat'
+            if ok then
+                return
             end
-        end,
-        { buffer = buffer, noremap = true, silent = true, desc = 'Format' }
-    )
+            vim.cmd 'Neoformat'
+        end
+        -- Try to use lsp formatter first, if it fails, use neoformat
+    end, { buffer = buffer, noremap = true, silent = true, desc = 'Format' })
 end
 
 function M.on_attach(client, buffer)
@@ -88,8 +74,8 @@ function M.setup(_)
         'jsonls',
         'ts_ls',
         'tailwindcss',
-        'powershell_es',
         'pyright',
+        'sqlls',
         'texlab',
         'clangd',
         'volar',
@@ -126,7 +112,7 @@ function M.setup(_)
         on_attach = M.on_attach,
         capabilities = M.capabilities(),
         -- bundle is at ~/repos/powershell-es
-        bundle_path = vim.fn.expand('~/repos/powershell-es'),
+        bundle_path = vim.fn.expand '~/repos/powershell-es',
     }
     -- Custom servers --
     local custom_servers = {
