aa5ad82b95ba312ae00ea6aa25bffd686fd7fe09
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-05 16:45:33 +0100

    refactor: rename manager as lazy, rename autocmd as autocmds

--- init.lua
+++ init.lua
@@ -2,20 +2,14 @@ vim.loader.enable()
 
 local core_modules = {
     "config.options",
-    "config.autocmd",
+    "config.autocmds",
     "config.keymaps",
+    "config.lazy"
 }
 
 for _, module in ipairs(core_modules) do
     local ok, err = pcall(require, module)
     if not ok then
         vim.notify("Error loading " .. module .. "\n\n" .. err)
-    end
-end
-
-if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/manager.lua") == 1 then
-    local ok, err = pcall(require, "manager")
-    if not ok then
-        vim.notify("Error loading package manager\n\n" .. err)
     end
 end
