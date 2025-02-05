e034adf65d282b173b8e87c903dc96704523cde7
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-05 23:32:02 +0100

    feat: made lazydev work

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,17 @@ return {
             -- Rust
             --
             -- Neovim dev
-            'folke/lazydev.nvim',
+            {
+                'folke/lazydev.nvim',
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
             -- Navbuddy
             {
                 "SmiteshP/nvim-navbuddy",
