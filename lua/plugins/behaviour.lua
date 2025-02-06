35127fe23e33c6ab41aee9eb69539501a5366d11
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-06 12:25:53 +0100

    feat: lazy load multicursor

--- lua/plugins/behaviour.lua
+++ lua/plugins/behaviour.lua
@@ -83,6 +83,7 @@ return {
     {
         "jake-stewart/multicursor.nvim",
         branch = "1.0",
+        event = "VeryLazy",
         config = function()
             local mc = require("multicursor-nvim")
 
