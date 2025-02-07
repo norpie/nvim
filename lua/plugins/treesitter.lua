8d35625441a4650e6ada07f04b888cf006b3714e
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-07 13:52:58 +0100

    feat: change colors for blankline and rainbowdelimit

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,27 +18,15 @@ return
                 lazy = false,
                 main = "rainbow-delimiters.setup",
                 opts = {
-                    -- strategy = {
-                    --     [''] = rainbow_delimiters.strategy['global'],
-                    --     vim = rainbow_delimiters.strategy['local'],
-                    -- },
-                    -- query = {
-                    --     [''] = 'rainbow-delimiters',
-                    --     lua = 'rainbow-blocks',
-                    -- },
-                    -- priority = {
-                    --     [''] = 110,
-                    --     lua = 210,
-                    -- },
-                    -- highlight = {
-                    --     'RainbowDelimiterRed',
-                    --     'RainbowDelimiterYellow',
-                    --     'RainbowDelimiterBlue',
-                    --     'RainbowDelimiterOrange',
-                    --     'RainbowDelimiterGreen',
-                    --     'RainbowDelimiterViolet',
-                    --     'RainbowDelimiterCyan',
-                    -- },
+                    highlight = {
+                        'RainbowDelimiterYellow',
+                        'RainbowDelimiterBlue',
+                        'RainbowDelimiterOrange',
+                        'RainbowDelimiterGreen',
+                        'RainbowDelimiterViolet',
+                        'RainbowDelimiterCyan',
+                        'RainbowDelimiterRed',
+                    },
                 }
             },
         },
