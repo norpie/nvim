2bc08d9a4b22bb52d27e808a6775ffc411a7a483
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-07 01:04:20 +0100

    feat: remove useless plugin

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -36,7 +36,6 @@ return {
         dependencies = {
             "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
-            "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter",
             "olimorris/neotest-phpunit",
             "andythigpen/nvim-coverage"
