e5c825552b2e28aeea0de2f1ccf7c1e48bd1847c
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-08-06 10:05:06 +0200

    feat: don't steal go to definition on every file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -260,10 +260,11 @@ return {
             "nvim-lua/plenary.nvim",
             { "vuki656/package-info.nvim", opts = {} },
         },
+        lazy = true,
         ft = { 'ts', 'tsx', 'js', 'jsx', 'svelte' },
-        keys = {
-            { "gd", "<cmd>TSToolsGoToSourceDefinition<CR>", desc = "Go to source definition", }
-        },
+        config = function()
+            vim.keymap.set('n', 'gd', '<cmd>TypescriptToolsGoToSourceDefinition<CR>', { desc = 'Go to source definition' })
+        end,
         opts = {}
     },
 }
