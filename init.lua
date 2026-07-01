970f13b742cb056618cd1f02768a34232b1dd345
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-01 10:37:10 +0200

    feat: set init path globally

--- init.lua
+++ init.lua
@@ -3,3 +3,8 @@
 local T = require("lib")
 
 vim.loader.enable()
+
+-- Set our location
+local src = debug.getinfo(1, "S").source
+local path = src:sub(1, 1) == "@" and src:sub(2) or nil
+vim.g.init_lua_path = path and vim.fs.normalize(vim.uv.fs_realpath(path) or path)
