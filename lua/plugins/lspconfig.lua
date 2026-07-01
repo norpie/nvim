8dd9fe0a4a27431b4e9b8de7fb6236b0d8da4638
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-01 21:52:41 +0200

    feat: add lspconfig and formatting on save

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,3 @@
+require('lib.packages').add({
+    'neovim/nvim-lspconfig',
+})
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,4 +1,35 @@
-local lua_ls = require("lsp.lua_ls")
+require('plugins.lspconfig')
 
-vim.lsp.config('lua_ls', lua_ls) 
-vim.lsp.enable('lua_ls')
+local lsps = {
+    'lua_ls',
+    'bashls',
+}
+
+for _, lsp in ipairs(lsps) do
+    local ok, config = pcall(require, 'lsp.' .. lsp)
+    if ok then
+        vim.lsp.config(lsp, config)
+    end
+    vim.lsp.enable(lsp)
+end
+
+-- formatting
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('LspFormatting', {}),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then
+            return
+        end
+        if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_clear_autocmds({ group = 'LspFormatting', buffer = args.buf })
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = 'LspFormatting',
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf })
+                end,
+            })
+        end
+    end,
+})
