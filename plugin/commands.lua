8bfa15c423412f5312399011465c7c5ce1558bdd
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-01 20:53:27 +0200

    feat: add update command

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -1,9 +1,8 @@
 -- register custom :reload command to source config
 vim.api.nvim_create_user_command('Reload', function()
-  for name, _ in pairs(package.loaded) do
-    if name:match('^user') then
-      package.loaded[name] = nil
-    end
-  end
   vim.cmd('source $CFG')
+end, {})
+
+vim.api.nvim_create_user_command('Update', function()
+    vim.pack.update()
 end, {})
