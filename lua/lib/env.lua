9141413524a5f9e782812e0d2a4ad9348a2a6cc2
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-01 10:37:15 +0200

    feat: config path utils

--- lua/lib/env.lua
+++ lua/lib/env.lua
@@ -18,4 +18,22 @@ M.load = function()
     is_loaded = true
 end
 
+M.config_dir = function()
+    local init_path = vim.g.init_lua_path
+    return vim.fn.fnamemodify(init_path, ":h")
+end
+
+-- Subtract the config directory from the given path and return a relative path, e.g. "/home/user/.config/nvim/lua/plugins/blink.lua" -> "lua/plugins/blink.lua"
+M.relative_path = function(path)
+    local config_dir = M.config_dir()
+    return vim.fn.fnamemodify(path, ":~:.")
+end
+
+-- Subtract the config directory and the "lua" directory from the given path and return a relative path, e.g. "/home/user/.config/nvim/lua/plugins/blink.lua" -> "plugins/blink.lua"
+M.relative_lua_path = function(path)
+    local config_dir = M.config_dir()
+    local lua_dir = config_dir .. "/lua"
+    return vim.fn.fnamemodify(path, ":~:."):gsub("^lua/", "")
+end
+
 return M
