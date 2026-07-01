a9485a817a997e6353da4339916913ba9a5f59cd
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-01 09:31:07 +0200

    feat: add custom vim.pack.add wrapper

--- lua/lib/init.lua
+++ lua/lib/init.lua
@@ -3,5 +3,6 @@ local M = {}
 M.env = require('lib.env')
 M.nvim = require('lib.nvim')
 M.repos = require('lib.repos')
+M.packages = require('lib.packages')
 
 return M
---
+++ lua/lib/packages.lua
@@ -0,0 +1,54 @@
+local M = {}
+
+local default_spec = {
+    branch = 'main',
+    platform = 'github',
+}
+
+local default_opts = {
+    confirm = false,
+}
+
+-- end form should always be a list of tables, even if it's just one table, even when coming from a mix of strings and tables, so we normalize the input to always be a list of tables
+local function normalize_specs(spec)
+    if type(spec) == 'string' then
+        return { { spec } }
+    elseif type(spec) == 'table' then
+        if #spec == 0 then
+            return { spec }
+        else
+            local normalized = {}
+            for _, s in ipairs(spec) do
+                if type(s) == 'string' then
+                    table.insert(normalized, { s })
+                elseif type(s) == 'table' then
+                    table.insert(normalized, s)
+                end
+            end
+            return normalized
+        end
+    else
+        error('Invalid spec type: ' .. type(spec))
+    end
+end
+
+-- use vim.pack.add to add a package to the runtime path
+-- using custom repository helper
+function M.add(specs)
+    specs = normalize_specs(specs)
+    for _, spec in ipairs(specs) do
+        local repo = spec[1]
+        local platform = spec.platform or default_spec.platform
+        local src = 'https://' .. platform .. '.com/' .. repo
+        local branch = spec.branch
+        local built_spec = {src = src, version = branch}
+        for k, v in pairs(spec) do
+            if k ~= 1 and k ~= 'branch' and k ~= 'platform' then
+                built_spec[k] = v
+            end
+        end
+        vim.pack.add({built_spec}, default_opts)
+    end
+end
+
+return M
