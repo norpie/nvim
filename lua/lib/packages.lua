5a03cc8ed2eed741a682eb1d8217978450336ba7
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-01 22:07:30 +0200

    feat: setup build system in packages

--- lua/lib/packages.lua
+++ lua/lib/packages.lua
@@ -32,12 +32,15 @@ local function normalize_specs(spec)
     end
 end
 
+local plugins_with_build = {}
+
 -- use vim.pack.add to add a package to the runtime path
 -- using custom repository helper
 function M.add(specs)
     specs = normalize_specs(specs)
     for _, spec in ipairs(specs) do
         local repo = spec[1]
+        local build = spec.build
         local platform = spec.platform or default_spec.platform
         local src = 'https://' .. platform .. '.com/' .. repo
         local branch = spec.branch
@@ -48,7 +51,35 @@ function M.add(specs)
             end
         end
         vim.pack.add({built_spec}, default_opts)
+        if build then
+            plugins_with_build[repo] = build
+        end
     end
 end
+
+local function build_plugin(repo, build)
+    -- something like this: nvim/site/pack/core/opt/telescope-fzf-native.nvim
+    local repo_name = repo:match('.*/(.*)')
+    local plugin_path = vim.fn.stdpath('data') .. '/site/pack/core/opt/' .. repo_name
+    if vim.fn.isdirectory(plugin_path) == 1 then
+        if type(build) == 'string' then
+            local build_cmd = vim.split(build, ' ')
+            vim.system(build_cmd, { cwd = plugin_path })
+        elseif type(build) == 'function' then
+            build()
+        end
+    end
+end
+
+local function hooks()
+    for repo, build in pairs(plugins_with_build) do
+        build_plugin(repo, build)
+    end
+end
+
+vim.api.nvim_create_autocmd('PackChanged', { callback = hooks })
+vim.api.nvim_create_user_command('PackBuild', function()
+    hooks()
+end, {})
 
 return M
