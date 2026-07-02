0d7a0853bb0d99710e05ab6df011011ba05c942b
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-03 00:42:14 +0200

    feat: blink colorful menu, ghost text, and avoid multiline overlap

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,10 +1,11 @@
 local env = require("lib.env")
 
-require("lib.packages").add({ 
+require("lib.packages").add({
     'saghen/blink.lib',
     'saghen/blink.cmp',
     -- snippets
     'rafamadriz/friendly-snippets',
+    'xzbdmw/colorful-menu.nvim',
 })
 
 local cmp = require('blink.cmp')
@@ -45,7 +46,43 @@ end
 local opts = {
     keymap = { preset = 'super-tab' },
 
-    completion = { documentation = { auto_show = true } },
+    completion = {
+        documentation = { auto_show = true },
+        ghost_text = { enabled = true },
+        menu = {
+            draw = {
+                -- We don't need label_description now because label and label_description are already
+                -- combined together in label by colorful-menu.nvim.
+                columns = { { "kind_icon" }, { "label", gap = 1 } },
+                components = {
+                    label = {
+                        text = function(ctx)
+                            return require("colorful-menu").blink_components_text(ctx)
+                        end,
+                        highlight = function(ctx)
+                            return require("colorful-menu").blink_components_highlight(ctx)
+                        end,
+                    },
+                },
+            },
+            direction_priority = function()
+                local ctx = require('blink.cmp').get_context()
+                local item = require('blink.cmp').get_selected_item()
+                if ctx == nil or item == nil then return { 's', 'n' } end
+
+                local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
+                local is_multi_line = item_text:find('\n') ~= nil
+
+                -- after showing the menu upwards, we want to maintain that direction
+                -- until we re-open the menu, so store the context id in a global variable
+                if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
+                    vim.g.blink_cmp_upwards_ctx_id = ctx.id
+                    return { 'n', 's' }
+                end
+                return { 's', 'n' }
+            end,
+        },
+    },
 
     sources = sources,
 
