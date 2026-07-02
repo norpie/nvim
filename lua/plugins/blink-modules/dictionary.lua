07a120230bccc4e1d307327ad718e8a0d156bdb9
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-02 15:50:17 +0200

    feat: disable dictionary

--- lua/plugins/blink-modules/dictionary.lua
+++ lua/plugins/blink-modules/dictionary.lua
@@ -4,18 +4,18 @@ local M = {}
 
 function M.providers()
     local env = require('lib.env')
-    return { 
-        dictionary = {
-            module = 'blink-cmp-dictionary',
-            name = 'Dict',
-            min_keyword_length = 1,
-            opts = {
-                force_fallback = true,
-                dictionary_files = {
-                    env.config_dir() .. '/words.txt'
-                }
-            }
-        }
+    return {
+        --dictionary = {
+        --    module = 'blink-cmp-dictionary',
+        --    name = 'Dict',
+        --    min_keyword_length = 1,
+        --    opts = {
+        --        force_fallback = true,
+        --        dictionary_files = {
+        --            env.config_dir() .. '/words.txt'
+        --        }
+        --    }
+        --}
     }
 end
 
