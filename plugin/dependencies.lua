6c6a648efa5ced006fac1d652ca3e0364aea3cc3
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-03 10:22:19 +0200

    feat: add devicons

--- plugin/dependencies.lua
+++ plugin/dependencies.lua
@@ -1,5 +1,6 @@
 require('lib.packages').add({
     'nvim-lua/plenary.nvim',
     'lewis6991/async.nvim',
-    'MunifTanjim/nui.nvim'
+    'MunifTanjim/nui.nvim',
+    'nvim-tree/nvim-web-devicons'
 })
