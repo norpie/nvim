a158a0434599f92ad7a99cb07dc85feaef6c9adb
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-04 21:11:03 +0200

    feat: lspkind

--- plugin/dependencies.lua
+++ plugin/dependencies.lua
@@ -2,5 +2,6 @@ require('lib.packages').add({
     'nvim-lua/plenary.nvim',
     'lewis6991/async.nvim',
     'MunifTanjim/nui.nvim',
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
+    'onsails/lspkind.nvim'
 })
