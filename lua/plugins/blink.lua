53a6721ac9a8fc0c58307204bf9bb42dd4231262
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2026-07-04 21:11:08 +0200

    feat: better blink icons

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -69,6 +69,31 @@ local opts = {
                 -- combined together in label by colorful-menu.nvim.
                 columns = { { "kind_icon" }, { "label", gap = 1 } },
                 components = {
+                    kind_icon = {
+                        text = function(ctx)
+                            local icon = ctx.kind_icon
+                            if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                                if dev_icon then
+                                    icon = dev_icon
+                                end
+                            else
+                                icon = require("lspkind").symbol_map[ctx.kind] or ""
+                            end
+
+                            return icon .. ctx.icon_gap
+                        end,
+                        highlight = function(ctx)
+                            local hl = ctx.kind_hl
+                            if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                                if dev_icon then
+                                    hl = dev_hl
+                                end
+                            end
+                            return hl
+                        end,
+                    },
                     label = {
                         text = function(ctx)
                             return require("colorful-menu").blink_components_text(ctx)
